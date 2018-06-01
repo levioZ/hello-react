cc865f3f189cdf21a73a216125a41daf274323b5
eli li <elili@objectivasoftware.com>
2018-06-01 10:56:58 +0800

    组件的组合,嵌套和组件树

--- src/index.js
+++ src/index.js
@@ -2,20 +2,68 @@ import React,{ Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+
+class Title extends Component
+{
+    render(){
+        return(
+            <h1>hello react</h1>
+        )
+
+    }
+}
+
+
 class Header extends Component{
 
     render(){
         return(
             <div>
-                <h1>hello react</h1>
+              <Title/>
+                <h2>This is header</h2>
             </div>
         )
     }
 }
 
+class Main extends Component
+{
+    render(){
+        return(
+            <div>
+                <h2>This is main Content</h2>
+            </div>
+        )
+    }
+}
+
+class Footer extends Component {
+    render () {
+        return (
+            <div>
+                <h2>This is footer</h2>
+            </div>
+        )
+    }
+}
+
+class Index extends Component
+{
+    render(){
+        return(
+            <div>
+                <Header/>
+                <Main/>
+                <Footer/>
+            </div>
+        )
+    }
+
+}
+
 
 ReactDOM.render(
-    <Header/>,
+    <Index/>,
     document.getElementById('root')
 );
 
